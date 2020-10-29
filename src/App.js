9d3ffd8acd5e6fc5b096b9fed4846bcb4a1621ff
IosifDobos <c16735789@mydit.ie>
2020-10-29 08:57:46 +0000

    Setup React Router Dom
    created react-router-dom for  e-commerce app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// Import additional components
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+
+//components
 export default function App() {
-  return <h1>hello from app component</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route
+          path="/product/:id"
+          children={<ProductDetails />}
+        >
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
