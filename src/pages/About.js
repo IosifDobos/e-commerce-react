200170186c7b31e1e1387c7a79e5551832a8653a
IosifDobos <c16735789@mydit.ie>
2020-10-29 09:53:19 +0000

    Update About.js
    Added lorem ispum text to the about page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Proin tempus eu arcu eget fringilla. Etiam ac pulvinar ex.
+        Proin vel accumsan nisl, vel faucibus nulla. Sed porttitor
+        eu sapien sed suscipit. Vivamus et lacus lectus.
+        Etiam elementum tellus id augue ullamcorper faucibus.
+        Donec imperdiet ligula enim, at aliquam erat ultricies a.
+        Integer consequat vestibulum ipsum ut lacinia. Sed consequat
+        non tellus id cursus. Duis mattis, velit et sollicitudin blandit,
+        libero turpis mollis felis, a elementum magna lacus id est.
+        Mauris iaculis nibh sit amet tellus feugiat fermentum.
+        Morbi sagittis mauris in tortor interdum volutpat.
+      </p>
+    </section>
+  )
 }
